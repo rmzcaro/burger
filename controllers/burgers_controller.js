341f7fd0c443b70c5b7d667881dd2941fff62948
Carolina RC <caro.rmz315@gmail.com>
2018-05-03 13:59:37 -0500

    created route to add a burger and update that burger is devoured

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,17 +6,39 @@ var router = express.Router();
 
 // create routes
 
-router.get("/", function(req, res) {
-    burger.seeAll(function(data) {
-        var hbsObj = { burgers: data
+router.get("/", function (req, res) {
+    burger.seeAll(function (data) {
+        var hbsObj = {
+            burgers: data
         };
         console.log(hbsObj);
         res.render("index", hbsObj)
     });
 });
-router.post("/api/cats", function(req,res) {
-    burger.create(["burger_name"], [req.body.burger_name])
-})
+router.post("/api/cats", function (req, res) {
+    burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
+        // send back the id of the new burger
+        res.json({
+            id: result.insertId
+        });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // if now rows were changed then id must not exist so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 // export
-module.exports = router; 
+module.exports = router;
