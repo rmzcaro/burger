27e931cca67ae43a022d0291ca6bf39ed8545e48
Carolina RC <caro.rmz315@gmail.com>
2018-05-03 11:59:57 -0500

    added cde to serve static content, port, and listener

--- config/orm.js
+++ config/orm.js
@@ -2,8 +2,31 @@ var connection = require("./connection.js");
 
 // methods that will execute the mysql commands in the controllers 
 // methods to retrieve and store data in burgers_db 
-// selectAll()
-// insertOne()
-// updateOne()
-
-// export the ORM object in module.exports 
+var orm = {
+    // select all burgers to show up on page 
+    selectAll: function (tableIput, colToSearch, valOfCol){
+        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+        connection.query(queryString, [tableIput, colToSearch, valOfCol], function(err, result) {
+            if (err) throw err; 
+            console.log(result);
+        });
+    }, 
+    // insert a new burger into db 
+insertOne: function(table, newBurger) {
+    var queryString = "INSERT INTO ?? VALUES ?"; 
+    connection.query(queryString, [table, newBurger], function(err, result){
+        if (err) throw err; 
+        console.log(result);
+    });
+}, 
+// when burger devoured, update the db 
+updateOne: function(table, devoured, newVal, devoured){
+    var queryString = "UPDATE ?? SET ?? = ? WHERE ??";
+     connection.query(queryString, [], function(err, result) {
+         if (err) throw err; 
+         console.log(result)
+     })
+} 
+}
+// export the ORM object in module.exports s
+module.exports = orm; 
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var port = process.env.PORT || 8000; 
+
 var app = express();
 
 var mysql = require('mysql');
+
+// serve static content for the app from the public directory 
+app.use(express.static("public"));
+// parse (ie to encode / decode)
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+var routes = require("./controllers/burgers_controller.js")
+
+app.listen(port, function() {
+    console.log("The app is listening on port " + port);
+});
