64b82bbb3b15a11fbb2ef5f586f4cd932289114a
Carolina RC <caro.rmz315@gmail.com>
2018-05-03 12:02:02 -0500

    added hbs

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(express.static("public"));
 // parse (ie to encode / decode)
 app.use(bodyParser.urlencoded({extended: true}));
 
+// parsee application (json)
+app.use(bodyParser.json());
 
+// set handlebars 
+var hbs = require("express-handlebars");
+
+app.engine("handlebars", hbs({defaultLayout:"main"}));
+app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js")
 
