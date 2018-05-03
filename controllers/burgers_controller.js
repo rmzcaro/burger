e9a11ef90cc3ed89e555d093eb0e0feee35c021a
Carolina RC <caro.rmz315@gmail.com>
2018-05-03 14:04:02 -0500

    added comment

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,6 +6,7 @@ var router = express.Router();
 
 // create routes
 
+// get the burger
 router.get("/", function (req, res) {
     burger.seeAll(function (data) {
         var hbsObj = {
@@ -15,7 +16,9 @@ router.get("/", function (req, res) {
         res.render("index", hbsObj)
     });
 });
+// ad a burger
 router.post("/api/cats", function (req, res) {
+    //add a burger name and whether its devoured
     burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function (result) {
         // send back the id of the new burger
         res.json({
@@ -28,12 +31,28 @@ router.put("/api/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
 
+    //change if burger devoured or not 
     burger.update({
         devoured: req.body.devoured
-    }, condition, function(result) {
+    }, condition, function (result) {
         if (result.changedRows == 0) {
             // if now rows were changed then id must not exist so 404
             return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// remove burger
+router.delete("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (req, res) {
+        if (result.affectedRows == 0) {
+            // if no rows changed, then error
+            return res.status(404).end();
+
         } else {
             res.status(200).end();
         }
