43bd1471a5ac37a22095c4179a48c58c46b0e0c8
Carolina RC <caro.rmz315@gmail.com>
2018-06-22 20:47:43 -0500

    created devour buttons

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,7 @@
+$(function() {
+    $(".change-devour").on("click", function(event) {
+        // var id = $(this).data("id");
+        // var newDevour = $(this).data("")
+        console.log("changed to devour!");
+    })
+})
